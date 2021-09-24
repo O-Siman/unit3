5fa44dfdfc1b7a2a642c9a3828ffb5ab550bd43b
O-Siman <72103629+O-Siman@users.noreply.github.com>
2021-09-24 11:52:03 -0400

    Better

--- unit3/src/com/columbushs/unit3/Unit3_4ElseIf.java
+++ unit3/src/com/columbushs/unit3/Unit3_4ElseIf.java
@@ -3,28 +3,27 @@ package com.columbushs.unit3;
 import java.util.Scanner;
 
 public class Unit3_4ElseIf {
-	
 	public void saySomething() {
-		
 		Scanner inputInfo = new Scanner(System.in);
 		System.out.print("What do you want to say?");
 		String talking = inputInfo.next();
 		talking += inputInfo.nextLine();
-		
-		//else ifs are all connected. 
-		//If the first one triggers the others do not even if they are true.
-		
-		if (talking.length()>=2) {
+
+		// else ifs are all connected.
+		// If the first one triggers the others do not even if they are true.
+
+		if (talking.length() >= 2) {
 			System.out.println("Thank you for saying something.");
-			System.out.println("This is what you said: "+talking);
-		} else if (talking.length()>=2 && !talking.equals("applesauce")) {
+			System.out.println("This is what you said: " + talking);
+		} else {
+			System.out.println("Please say more than 1 letter.");
+		}
+
+		if (talking.length() >= 2 && !talking.equals("applesauce")) {
 			System.out.println("You have not said the secret word. Try again.");
 			saySomething();
-		} else if (talking.length()>=2 && talking.equals("applesauce")) {
+		} else if (talking.equals("applesauce")) { // This clause is unnecessary anyways
 			System.out.println("You have said the secret word. Congratulations!!!");
-		} 
-		
-		
+		}
 	}
-
 }
