0fcf13fa981c82de32c609efd26a844ddde119da
O-Siman <72103629+O-Siman@users.noreply.github.com>
2021-09-24 11:52:03 -0400

    Better

--- unit3/src/com/columbushs/unit3/Unit3_2IfStatements.java
+++ unit3/src/com/columbushs/unit3/Unit3_2IfStatements.java
@@ -3,39 +3,33 @@ package com.columbushs.unit3;
 import java.util.Scanner;
 
 public class Unit3_2IfStatements {
-	
-	
 	public void ifTesting() {
-		
-		//if (something is true) {Do this}
-		
-		int howOld = 45;
+		// if (something is true) {Do this}
+
+		int howOld = 35;
 		System.out.println("Are you old?");
-		
-		if (howOld>=40) {
+
+		if (howOld >= 40) {
 			System.out.println("Yes you're old.");
 		}
 	}
-		
-	
 
-	
+
 	public void saySomething() {
-		
 		Scanner inputInfo = new Scanner(System.in);
 		System.out.print("What do you want to say?");
 		String talking = inputInfo.next();
 		talking += inputInfo.nextLine();
-		
-		if (talking.length()<=2) {
+
+		if (talking.length() < 2) {
 			System.out.println("You must say more than a letter.");
-			
+			saySomething();
 		}
-		if (talking.length()>=2) {
+		if (talking.length() >= 2) {
 			System.out.println("Thank you for saying something.");
-			System.out.println("This is what you said: "+talking);
+			System.out.println("This is what you said: " + talking);
 		}
-		
+
 //		if (talking.length()>=2 && !talking.equals("applesauce")) {
 //			System.out.println("You have not said the secret word. Try again.");
 //			saySomething();
@@ -43,8 +37,5 @@ public class Unit3_2IfStatements {
 //		if (talking.length()>=2 && talking.equals("applesauce")) {
 //			System.out.println("You have said the secret word. Congratulations!!!");
 //		}
-		
-		
 	}
-
 }
--- unit3/src/com/columbushs/unit3/Unit3_3IfElse.java
+++ unit3/src/com/columbushs/unit3/Unit3_3IfElse.java
@@ -1,15 +1,13 @@
 package com.columbushs.unit3;
 
 public class Unit3_3IfElse {
-	
-public void ifTesting() {
-		
-		//if (something is true) {Do this} else {do this}
-		
+	public void ifTesting() {
+		// if (something is true) {Do this} else {do this}
+
 		int howOld = 45;
 		System.out.println("Are you old?");
-		
-		if (howOld>=40) {
+
+		if (howOld >= 40) {
 			System.out.println("Yes you're old.");
 		} else {
 			//else is the catch all.
